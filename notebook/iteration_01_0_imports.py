e1228d39ecb2bc5a647bf82bb6badc31df7d55c6
ReneMarotBDBA62 <94298282+ReneMarotBDBA62@users.noreply.github.com>
2021-11-24 20:08:09 -0500

    V 2021-11-24 20h07

---
+++ notebook/iteration_01_0_imports.py
@@ -0,0 +1,12 @@
+from datetime import datetime 
+from PIL import Image
+from tqdm import tqdm
+import cv2
+import glob
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+import pickle
+import random
+import seaborn as sns
