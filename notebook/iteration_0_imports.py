12c0abb023e63bc79821f01c6dbc80e522622a28
kbenmus02 <mkbensmaia@gmail.com>
2021-11-30 20:47:38 -0500

    push RF

---
+++ notebook/iteration_0_imports.py
@@ -0,0 +1,14 @@
+# 2021-11-24 20h43
+from datetime import datetime 
+from PIL import Image
+from tqdm import tqdm
+import cv2
+import glob
+import gzip #http://henrysmac.org/blog/2010/3/15/python-pickle-example-including-gzip-for-compression.html
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+import pickle
+import random
+import seaborn as sns
